6edfa85f42cd18de9cbc82a8aef50df54baa2197
AlliageSphere <alliagesphere@live.fr>
2020-11-03 10:53:21 +0100

    Un bouton cliquable.

--- mon-app/src/components/ASButton.js
+++ mon-app/src/components/ASButton.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { Component } from 'react';
 
 export class ASButton extends React.Component
 {
+	constructor(props)
+	{
+		super(props);
+		this.helloThere = this.helloThere.bind(this);
+	}
+
+	helloThere()
+	{
+		alert('Hi! Admin');
+	}
+	
 	render()
 	{
-		return
-		(
-			<div>
-				<button type="button">Clique-moi !</button>
+		return(
+			<div className="divButton">
+				<button type="button" onClick={this.helloThere}>Clique-moi !</button>
 			</div>
-			
 		)
 	}
 }
